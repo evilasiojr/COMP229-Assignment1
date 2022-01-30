99f1d4962a4fd26bab732be8ada6b3398422e335
EvilasioJr <evilasiojunior@hotmail.com>
2022-01-30 12:46:39 -0500

    main

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/business_contact_list"
+    "URI": "mongodb+srv://evilasio:2iA4w9gt4SchWmxX@cluster0.z5qbn.mongodb.net/business_contact_list?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
